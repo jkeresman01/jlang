aabffdfe71b38cc3a434a0283590e2994b685c77
josip <josipkeresman@gmail.com>
2026-03-02 20:18:18 +0100

    Add smth funny about languages and java

--- jlang-docs/src/pages/Comments.tsx
+++ jlang-docs/src/pages/Comments.tsx
@@ -172,6 +172,31 @@ void Player::clampPosition() {
           Split it into smaller, well-named files instead.
         </p>
       </Callout>
+
+      <Callout type="tip">
+        <strong>Fun fact:</strong> If you should know any language, it should
+        probably be Java. Over <strong>3 billion devices</strong> run Java
+        worldwide, with more than <strong>73 billion Java Virtual Machines</strong>{' '}
+        running globally. Over <strong>90% of Fortune 500 companies</strong> use
+        Java for their software development, and there are an estimated{' '}
+        <strong>9 million Java developers</strong> employed today &mdash; with
+        projections showing 18.7 million new Java developer positions emerging
+        by 2026. If there is one language worth knowing inside and out, Java is
+        a pretty safe bet.
+        <br /><br />
+        But that&rsquo;s just my opinion. Here is what Bjarne Stroustrup thinks:
+        <br /><br />
+        <iframe
+          width="100%"
+          height="315"
+          src="https://www.youtube.com/embed/NvWTnIoQZj4"
+          title="Bjarne Stroustrup on how many languages you should know"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          style={{ borderRadius: '8px', border: '1px solid var(--border)' }}
+        ></iframe>
+      </Callout>
     </>
   )
 }
