1b0ad4a035bd5ba6507d7b508f5e703a593aefd4
josip <josipkeresman@gmail.com>
2026-04-08 00:10:35 +0200

    Give honest opinion on electrical vs software engineering

--- jlang-docs/src/pages/Preface.tsx
+++ jlang-docs/src/pages/Preface.tsx
@@ -131,7 +131,9 @@ export default function Preface() {
         anyhow back to me. I remembered myself at their age. During my first two 
         years of university I was a complete mess, uninterested in anything. I would 
         sit through a lecture or not come to any of them and just show up on exam 
-        day (Electrical engineering ones, software engineering was more interesting to me). 
+        day (Electrical engineering ones, software engineering was more interesting to me,
+        but that's just my opinion, have huge respect for both disciplines tough and 
+        electrical engineering is arguably more difficult). 
         I realised some years later that I should have been listening better. 
         I got my things together in the final year, but I can see similar patterns 
         in other young fellas &mdash; we all have a moment where something starts to 
