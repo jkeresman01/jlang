d0eb8b9a1fa63a1d0a789b2059a3ac61c97d55a2
Josip Keresman <josipkeresman@gmail.com>
2026-04-21 13:54:35 +0200

    Improve some wording about subjective realities
    Refactor text for clarity and correct grammatical errors.

--- jlang-docs/src/pages/Ending.tsx
+++ jlang-docs/src/pages/Ending.tsx
@@ -174,14 +174,14 @@ export default function Ending() {
          "An interesting tought about that, world is quite simple,
          unless people decide to make it complicated,
          we don't see it objectivly, but are instead traped in our own views 
-         in our own subjective reality and that's generaly fine, perhaps a fact is
-         missing for that to make sense. Perhaps one might think that one is a lie.
-         Perhaps they might be right. Perhaps people get afraid and defend their own views
-         altought it might be a lie. One thing is for sure, people who generaly 
-         don't care, get out of the debate. But generaly what I'm trying to say, people
-         live in their own subjective realities and little puzzle like this could have 
-         another meaning for them, while generaly it is a puzzle about a 
-         shoeseller or a milkseller."
+         in our own subjective reality, perhaps a fact is missing for that to make sense.
+         Perhaps one might think that one is a lie. Perhaps they might be right. 
+         Perhaps people get afraid and defend their own views altought it might be a lie.
+         One thing is for sure, people who generaly don't care, get out of the debate.
+         But generaly what I'm trying to say, people live in their own subjective realities 
+         and live there own stories and little puzzle like this could have 
+         another meaning for them, while generaly it is a puzzle about a shoeseller or 
+         a milkseller."
       </i></p>
     </Callout>
 
